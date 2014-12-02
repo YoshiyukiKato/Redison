7236a876327fd246f70a42270ef7c3b2f5aed38a
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-02 22:52:24 +0900

    separate test program

--- test/listener_test.js
+++ test/listener_test.js
@@ -1,11 +1,13 @@
+//test of initListener with arguments
+//and stopListener with arguments
+
 var Redison = require("../lib/redison");
 var subClient = Redison.redisonize().initListener("subscribe","message");
 
 console.log(subClient.listeners("subscribe"));
 console.log(subClient.listeners("message"));
 
-subClient.stopListener("subscribe");
-subClient.stopListener("message");
+subClient.stopListener("subscribe","message");
 
 console.log(subClient.listeners("subscribe"));
 console.log(subClient.listeners("message"));
--- test/sebPsub_test.js
+++ test/sebPsub_test.js
@@ -3,42 +3,6 @@ var Redison = require("./lib/Redison");
 var subClient = Redison.redisonize().initListener();
 var pubClient = Redison.redisonize();
 
-subClient.setSub({
-    test1:{
-        subscribe:function(channel,count){
-            console.log("Subscribing " + channel + " :: Now we have " + count + " channels");
-            pubClient.publish("test1", "test1::" + JSON.stringify({ message: "Single channel test."}));
-        },
-        message:function(channel,message){
-            console.log("Message from " + channel + " : "  + message);
-            subClient.stateUnsub(channel,false);
-            console.log("unsub this channel with pause state");
-            console.log(subClient.hasCallback(channel));
-        },
-        unsubscribe:function(channel,count){
-            console.log("Unsubscribed " + channel + " :: Now we have " + count + " channels");
-            console.log("***Single channel test was successfully ended.***");
-        },
-
-    },
-    test2:{
-        subscribe:function(channel,count){
-            console.log("Subscribing " + channel + " :: Now we have " + count + " channels");
-            pubClient.publish("test2", "test2::" + JSON.stringify({ message: "Multi channel test."}));
-        },
-        message:function(channel,message){
-            console.log("Message from " + channel + " : "  + message);
-            subClient.stateUnsub(channel,true);
-            console.log("unsub this channel with quit state");
-            console.log(subClient.hasCallback(channel));
-        },
-        unsubscribe:function(channel,count){
-            console.log("Unsubscribed " + channel + " :: Now we have " + count + " channels");
-            console.log("***Multi channel test was successfully ended.***");
-        }
-    }
-});
-
 subClient.setPsub({
 	"ptest*":{
 		psubscribe:function(pattern,count){
@@ -74,9 +38,8 @@ subClient.setPsub({
 	}
 });
 
-
 try{
-    subClient.setSub();
+    subClient.setPsub();
 }catch(e){
     console.log( e +" :: This error is intentionally occured. No problem.");
 }
